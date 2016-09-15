698ea498c52ea1b1d3db28666524ee8c3084bc3b
Adam Ballinger <crunchienut20@gmail.com>
2016-09-15 22:40:20 +0100

    Initial Commit.

---
+++ Assets/Scripts/LuaController.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class LuaController : MonoBehaviour
+    {
+
+        public Button runButton;
+        public InputField inputField;
+
+        private LuaInterpreter interp;
+
+        public void OnButtonClick()
+        {
+            var script = inputField.text;
+            interp = new LuaInterpreter(script);
+            Debug.Log(interp.Run().String);
+        }
+    }
+}
---
+++ Assets/Scripts/LuaInterpreter.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using MoonSharp.Interpreter;
+
+namespace Assets.Scripts
+{
+    public class LuaInterpreter
+    {
+
+        private Script script;
+
+        public LuaInterpreter(string _luaCode)
+        {
+            script = new Script();
+            script.DoString(_luaCode);
+        }
+
+        public DynValue Run()
+        {
+            var result = Script.RunString(script.GetSourceCode(1).Code);
+            return result;
+        }
+    }
+}
