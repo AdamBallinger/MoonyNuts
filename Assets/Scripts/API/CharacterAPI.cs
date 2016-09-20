fc9a22aae3d7b70181765fcc01c203fa96865e88
Adam Ballinger <crunchienut20@gmail.com>
2016-09-20 02:32:50 +0100

    Comments

--- Assets/Scripts/API/CharacterAPI.cs
+++ Assets/Scripts/API/CharacterAPI.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 
 namespace Assets.Scripts.API
 {
-    public class CharacterAPI
+    public static class CharacterAPI
     {
 
+        /// <summary>
+        /// A list of gameobjects that are registered to the CharacterAPI. Only objects in here will work with the LUA interpreter API calls.
+        /// </summary>
         private static List<GameObject> objects = new List<GameObject>();
 
+        /// <summary>
+        /// Adds a new character gameobject to the API.
+        /// </summary>
+        /// <param name="_object"></param>
         public static void AddObject(GameObject _object)
         {
             if(!objects.Contains(_object))
@@ -17,6 +24,11 @@ namespace Assets.Scripts.API
             }
         }
 
+        /// <summary>
+        /// Gets the ID for a given gameobject.
+        /// </summary>
+        /// <param name="_object"></param>
+        /// <returns></returns>
         public static int GetID(GameObject _object)
         {
             if(objects.Contains(_object))
@@ -28,6 +40,11 @@ namespace Assets.Scripts.API
             return -1;
         }
 
+        /// <summary>
+        /// Returns the API controller for the gameobject with the given ID, or null if no object exists with the given ID.
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <returns></returns>
         public static CharacterAPIController GetGameObject(int _id)
         {
             if(_id > objects.Count || _id < 0)
@@ -37,6 +54,5 @@ namespace Assets.Scripts.API
 
             return objects[_id].GetComponent<CharacterAPIController>();
         }
-
     }
 }
