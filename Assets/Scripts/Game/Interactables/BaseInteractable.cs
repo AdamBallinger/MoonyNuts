80d0cb0ccd73f89365d63210b034ae31fef0a032
Adam Ballinger <crunchienut20@gmail.com>
2017-03-18 16:25:02 +0000

    Base interactable class.

---
+++ Assets/Scripts/Game/Interactables/BaseInteractable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game.Interactables
+{
+    public abstract class BaseInteractable : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Called when something interacts with this components game object.
+        /// </summary>
+        /// <param name="_interactSource">The game object that interacted with this component's game object.</param>
+        public abstract void OnInteract(GameObject _interactSource);
+
+    }
+}
