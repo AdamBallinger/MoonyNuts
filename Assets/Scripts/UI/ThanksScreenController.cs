3ec00565f370018674fe933a6201fd4c77d50195
Adam Ballinger <crunchienut20@gmail.com>
2017-04-22 22:55:10 +0100

    added thanks scene

---
+++ Assets/Scripts/UI/ThanksScreenController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class ThanksScreenController : MonoBehaviour
+    {
+
+        public string questionnaireURL;
+
+        public void OnQuestionnaireButtonPress()
+        {
+            Application.OpenURL(questionnaireURL);
+        }
+
+        public void OnMenuButtonPress()
+        {
+            SceneManager.LoadScene("menu");
+        }
+
+        public void OnQuitButtonPress()
+        {
+            Application.Quit();
+        }
+    }
+}
